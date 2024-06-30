198b591529f08418f63b0bbd3138388e87ee0b78
OogieBoogie <egorov.2001@bk.ru>
2024-07-01 01:31:34 +0500

    fixinf com.facebook dependecies

--- android/src/main/java/expo/modules/bashgmuhce/BashgmuHCEModule.kt
+++ android/src/main/java/expo/modules/bashgmuhce/BashgmuHCEModule.kt
@@ -1,42 +1,35 @@
 package expo.modules.bashgmuhce
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NfcAdapter
+import android.nfc.NfcManager
+import android.util.Log
+
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.WritableMap
+
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 class BashgmuHCEModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
+
+  private val reactContext: Context
+    get() = appContext.reactContext ?: throw Exception("ReactContextLost")
   override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('BashgmuHCE')` in JavaScript.
     Name("BashgmuHCE")
-
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
-
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
 
     // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
     Function("hello") {
       "Hello world! 123 👋"
     }
 
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
+    Function("supportNFC") {
+      return@Function "supportNFC"
     }
 
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
     View(BashgmuHCEView::class) {
       // Defines a setter for the `name` prop.
       Prop("name") { view: BashgmuHCEView, prop: String ->
@@ -44,4 +37,22 @@ class BashgmuHCEModule : Module() {
       }
     }
   }
+
+  private fun supportNFC(): Unit{
+    val manager = this.reactContext.getSystemService(Context.NFC_SERVICE) as NfcManager
+    val adapter = manager.defaultAdapter
+
+    val map = Arguments.createMap()
+
+    if (adapter != null) {
+      map.putBoolean("supported", true)
+      map.putBoolean("enabled", adapter.isEnabled)
+    } else {
+      map.putBoolean("supported", false)
+      map.putBoolean("enabled", false)
+    }
+    return map
+
+  }
+
 }
--- example/App.tsx
+++ example/App.tsx
@@ -6,6 +6,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>{BashgmuHCE.hello()}</Text>
+
+      <Text>{JSON.stringify(BashgmuHCE.supportNFC())}</Text>
     </View>
   );
 }
